b26dd7232e683ec02b2e04435da438944caaca51
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-06 10:53:03 +0300

    Update and rename regex_match_03.cpp to regex_match/regex_match_03.cpp

--- regex_match/regex_match_03.cpp
+++ regex_match/regex_match_03.cpp
@@ -1,12 +1,25 @@
-#include <regex>
-#include "nutility.h"
+#include <vector>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <regex>
 
+using namespace std;
+
+vector<string> file_to_vec(const string& fname)
+{
+	ifstream ifs{ fname };
+	if (!ifs) {
+		cerr << "dosya acilamiyor\n";
+		throw runtime_error{ fname + " cannot be opened!" };
+	}
+
+	return vector<string> {istream_iterator<string>{ifs}, {}};
+}
 
 int main()
 {
-	using namespace std;
-	auto svec = get_dictionary("words.txt");
+	auto svec = file_to_vec("words.txt");
 	regex rgx{ ".*([ckprts]+[aeio]+).*([tcln]ion).*" };
 
 	smatch sm;
@@ -18,7 +31,7 @@ int main()
 			word.insert(word.begin() + sm.position(2) + 2, '[');
 			word.insert(word.begin() + sm.position(2) + 3 + sm.length(2), ']');
 			cout << word;
-			(void)getchar();			
+			(void)getchar();
 		}
 	}
 }
