3fa9c67e170058fe6fc770eee37fbedaf6a8cf0d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 14:35:51 +0300

    Create sregex_iterator_05.cpp

---
+++ sregex_iterator_05.cpp
@@ -0,0 +1,15 @@
+#include <regex>
+#include <string>
+#include <iostream>
+
+int main() 
+{
+    const std::string text = "Necati Ergin bugun Ankara'ya uctu. Yarin geri donecek";
+    const std::regex  re(R"(\b[A-Z]\w+\b)"); 
+
+    
+    for (std::sregex_iterator it{ text.begin(), text.end(), re }, end; it != end; ++it) {
+        const std::smatch& m = *it;               
+        std::cout << m.str() << " @pos " << m.position() << '\n';
+    }
+}
