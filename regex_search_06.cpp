278e3c6bda52442d3ba4daf0ef02e2356121928b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 14:52:37 +0300

    Create regex_search_06.cpp

---
+++ regex_search_06.cpp
@@ -0,0 +1,12 @@
+#include <regex>
+#include <string>
+#include <iostream>
+
+int main() 
+{
+    std::string str = "Code7 CodeX";
+    std::regex re(R"([A-Z]\w*(?=\d))");
+    std::smatch sm;
+    if (std::regex_search(str, sm, re))
+        std::cout << "Match: " << sm.str() << '\n'; // "Code"
+}
