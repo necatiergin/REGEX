4ab6e29d32ca9150fe4fec27170970da2f219bbd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 15:31:21 +0300

    Update regex_token_iterator_01.cpp

--- regex_token_iterator_01.cpp
+++ regex_token_iterator_01.cpp
@@ -4,35 +4,35 @@
 
 int main()
 {
-	using namespace std;
+	using iterator = std::sregex_token_iterator;
 
-	string str("projede projeyi basariya ulastiracak profesyonel programcilar gerekiyor");
-	regex rgx("\\b(pro)([^ ]*)");   
+	std::string str("projede projeyi basariya ulastiracak profesyonel programcilar gerekiyor");
+	std::regex rgx("\\b(pro)([^ ]*)");
 
-	sregex_token_iterator rend;
+	iterator rend;
 
-	cout << "uyanlarin tamami\n";
-	for (sregex_token_iterator iter(str.begin(), str.end(), rgx, 0); iter != rend; ++iter) 
-		std::cout << " [" << *iter << "]";
-	
-	cout << "\n\n";
-
-	cout << "2. submatch'ler:";
-	for (sregex_token_iterator iter(str.begin(), str.end(), rgx, 2); iter != rend; ++iter)
+	std::cout << "uyanlarin tamami\n";
+	for (iterator iter(str.begin(), str.end(), rgx, 0); iter != rend; ++iter)
 		std::cout << " [" << *iter << "]";
 
-	cout << "\n\n";
+	std::cout << "\n\n";
 
-	cout << "1. ve 2. submatch'ler:";
-	for (sregex_token_iterator iter(str.begin(), str.end(), rgx, { 1, 2 }); iter != rend; ++iter)
+	std::cout << "2. submatch'ler:";
+	for (iterator iter(str.begin(), str.end(), rgx, 2); iter != rend; ++iter)
 		std::cout << " [" << *iter << "]";
 
-	cout << "\n\n";
+	std::cout << "\n\n";
 
-	cout << "ayiraclar :";
-	
-	for (sregex_token_iterator iter(str.begin(), str.end(), rgx, -1); iter != rend; ++iter)
+	std::cout << "1. ve 2. submatch'ler:";
+	for (iterator iter(str.begin(), str.end(), rgx, { 1, 2 }); iter != rend; ++iter)
 		std::cout << " [" << *iter << "]";
 
-	cout << "\n\n";
+	std::cout << "\n\n";
+
+	std::cout << "ayiraclar :";
+
+	for (iterator iter(str.begin(), str.end(), rgx, -1); iter != rend; ++iter)
+		std::cout << " [" << *iter << "]";
+
+	std::cout << "\n\n";
 }
