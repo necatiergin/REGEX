14ebda99e0052ae84a0e1a4589945c74c19158d6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 15:41:35 +0300

    Create regex_token_iterator_03.cpp

---
+++ regex_token_iterator_03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+// Split function using regex_token_iterator
+std::vector<std::string> split(const std::string& input, const std::string& delim_pattern)
+{
+    std::regex rgx(delim_pattern);
+    std::sregex_token_iterator first{ input.begin(), input.end(), rgx, -1 };
+    std::sregex_token_iterator last;
+    return { first, last };
+}
+
+int main()
+{
+    std::string text = "apple,orange,,banana,kiwi,pear,melon,,cherry";
+    auto tokens = split(text, ",");
+
+    std::cout << "Tokens:\n";
+    for (auto& t : tokens)
+        if (!t.empty())
+            std::cout << "[" << t << "]\n";
+}
