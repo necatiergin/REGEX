f47601075b79bd54fcd600a87247fe44666038df
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 09:09:19 +0300

    Update mark_count_01.cpp

--- mark_count_01.cpp
+++ mark_count_01.cpp
@@ -3,26 +3,24 @@
 
 int main()
 {
-    using namespace std;
+    std::regex r1{ "necati" };
+    std::cout << "r1 icin alt ifade sayisi : " << r1.mark_count() << '\n';
 
-    regex r1{ "necati" };
-    cout << "r1 icin alt ifade sayisi : " << r1.mark_count() << "\n";
+    std::regex r2{ "necati (erg)in" };
+    std::cout << "r2 icin alt ifade sayisi : " << r2.mark_count() << '\n';
 
-    regex r2{ "necati (erg)in" };
-    cout << "r2 icin alt ifade sayisi : " << r2.mark_count() << "\n";
+    std::regex r3{ "(necati) (ergin)" };
+    std::cout << "r3 icin alt ifade sayisi : " << r3.mark_count() << '\n';
 
-    regex r3{ "(necati) (ergin)" };
-    cout << "r3 icin alt ifade sayisi : " << r3.mark_count() << "\n";
-    
-    regex r4{ "(nec(ati)rgin)" };
-    cout << "r4 icin alt ifade sayisi : " << r4.mark_count() << "\n";
+    std::regex r4{ "(nec(ati)rgin)" };
+    std::cout << "r4 icin alt ifade sayisi : " << r4.mark_count() << '\n';
 
-    regex r5{ "\\(necati\\)" };
-    cout << "r5 icin alt ifade sayisi : " << r5.mark_count() << "\n";
+    std::regex r5{ "\\(necati\\)" };
+    std::cout << "r5 icin alt ifade sayisi : " << r5.mark_count() << '\n';
 
-    regex r6{ "n(eca)ti", regex_constants::nosubs };
-    cout << "r6 icin alt ifade sayisi : " << r6.mark_count() << "\n";
+    std::regex r6{ "n(eca)ti", std::regex_constants::nosubs };
+    std::cout << "r6 icin alt ifade sayisi : " << r6.mark_count() << '\n';
 
-    regex r7{ "n(?:eca)ti"};
-    cout << "r7 icin alt ifade sayisi : " << r7.mark_count() << "\n";
+    std::regex r7{ "n(?:eca)ti" };
+    std::cout << "r7 icin alt ifade sayisi : " << r7.mark_count() << '\n';
 }
