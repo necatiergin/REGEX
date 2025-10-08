00a1f28b79bb9cbae126a90cb20697831d70c57e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-08 12:44:42 +0300

    Create regex_search_04.cpp

---
+++ regex_search_04.cpp
@@ -0,0 +1,18 @@
+// greedy or lazy
+
+#include <vector>
+#include <string>
+#include <regex>
+#include <iostream>
+
+int main() 
+{
+	std::string text = "<b>bold</b><i>italic</i>";
+	std::regex pattern("<.*>");
+	std::smatch match;
+	std::regex_search(text, match, pattern);
+	std::cout << match.str() << '\n';
+	pattern = "<.*?>";
+	std::regex_search(text, match, pattern);
+	std::cout << match.str() << '\n';
+}
