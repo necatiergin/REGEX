2087fb96d2f60c93ab143c86ebcbff84d1d2c9df
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-10 15:31:57 +0300

    Update sregex_iterator_04.cpp

--- sregex_iterator_04.cpp
+++ sregex_iterator_04.cpp
@@ -11,18 +11,16 @@ std::string file_to_str(const std::string& fname)
 		std::cerr << "dosya acilamiyor\n";
 		throw std::runtime_error{ fname + " cannot be opened!" };
 	}
-	return std::string (std::istreambuf_iterator<char>{ifs}, {});
+	return std::string(std::istreambuf_iterator<char>{ifs}, {});
 }
 
 int main()
 {
-	using namespace std;
-
 	auto s = file_to_str("tor.txt");
 
-	regex rgx{ R"(\w+tor\w+)" };
+	std::regex rgx{ R"(\w+tor\w+)" };
 
-	for (sregex_iterator iter{ s.begin(), s.end(), rgx }, end; iter != end; ++iter) {
-		cout << iter->str() << " " << iter->position() << '\n';
+	for (std::sregex_iterator iter{ s.begin(), s.end(), rgx }, end; iter != end; ++iter) {
+		std::cout << iter->str() << " " << iter->position() << '\n';
 	}
 }
