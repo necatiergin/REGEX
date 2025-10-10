a7573ee22f023cf06116ca6cdefca50566fd9ec5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-10 15:39:28 +0300

    Update regex_token_iterator_02.cpp

--- regex_token_iterator_02.cpp
+++ regex_token_iterator_02.cpp
@@ -4,13 +4,12 @@
 
 int main()
 {
-    using namespace std;
-    string s = "murat, kadir, kemal ve aysenur C++ ogrendiler";
-    
-    regex rgx{ "[\\s,]+" };
-    sregex_token_iterator iter{ s.begin(), s.end(), rgx, -1 };
-    sregex_token_iterator end;
+    std::string s = "murat, kadir, kemal ve aysenur C++ ogrendiler";
+
+    std::regex rgx{ "[\\s,]+" };
+    std::sregex_token_iterator iter{ s.begin(), s.end(), rgx, -1 };
+    std::sregex_token_iterator end;
     for (; iter != end; ++iter) {
-        std::cout << iter->str() << "\n";
+        std::cout << iter->str() << '\n';
     }
 }
